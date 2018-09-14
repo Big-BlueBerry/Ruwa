d56d16e65a8f83a3b9398bb6d9b8bc248842cc80
XxshiftxX <qjfrntop12@naver.com>
2018-09-14 17:16:40 +0900

    Initial Commit

---
+++ Assets/Scripts/IInputManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputManager
+{
+    bool IsKeyDown(int key);
+    bool IsKeyPressed(int key);
+    bool IsKeyReleased(int key);
+}
