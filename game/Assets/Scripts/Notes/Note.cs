b047e64adf92ab8bceaaf9f85aaaf38bec03324c
Sung-Gi-Hyeon <sunggh7130@gmail.com>
2018-11-09 15:40:44 +0900

    Modify directory

---
+++ game/Assets/Scripts/Notes/Note.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Note : MonoBehaviour
+{
+    public float Time { get; set; }
+    public int Position { get; set; }
+    public int Width { get; set; }
+}
